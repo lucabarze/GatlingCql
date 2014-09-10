e92225fa3ae6efb354553fa583cffda07ae5f01a
Mikhail Stepura <mikhail.stepura@outlook.com>
2014-09-10 14:08:53 -0700

    Logging

--- src/main/scala/com/github/gatling/cql/CqlRequestAction.scala
+++ src/main/scala/com/github/gatling/cql/CqlRequestAction.scala
@@ -1,33 +1,42 @@
 package com.github.gatling.cql
 
+import com.datastax.driver.core.ResultSet
+import com.typesafe.scalalogging.slf4j.StrictLogging
+
 import akka.actor.ActorRef
 import akka.actor.actorRef2Scala
 import io.gatling.core.action.Failable
 import io.gatling.core.action.Interruptable
+import io.gatling.core.result.message.KO
 import io.gatling.core.result.message.OK
 import io.gatling.core.result.writer.DataWriterClient
 import io.gatling.core.session.Session
 import io.gatling.core.util.TimeHelper.nowMillis
-import io.gatling.core.validation._
-import io.gatling.core.result.message.KO
+import io.gatling.core.validation.FailureWrapper
+import io.gatling.core.validation.SuccessWrapper
+import io.gatling.core.validation.Validation
 
-class CqlRequestAction(val next: ActorRef, protocol: CqlProtocol, attr: CqlAttributes) extends Interruptable with Failable with DataWriterClient {
+class CqlRequestAction(val next: ActorRef, protocol: CqlProtocol, attr: CqlAttributes) 
+  extends Interruptable 
+  with Failable 
+  with DataWriterClient 
+  with StrictLogging {
 
-  def executeOrFail(session: Session): Validation[Unit] = {
+  def executeOrFail(session: Session): Validation[ResultSet] = {
     val start = nowMillis
     val stmt = attr.statement(session)
     try {
-      protocol.session.execute(stmt)
-      writeRequestData(session, attr.tag, start, nowMillis, start, nowMillis, OK, None, Nil)
-      logger.trace("{}", stmt)
-      next ! session
-      Success()
+      val result = protocol.session.execute(stmt)
+      writeRequestData(session, attr.tag, start, nowMillis, session.startDate, nowMillis, OK, None, Nil)
+      logger.trace(s"$stmt")
+      next ! session.markAsSucceeded
+      result.success
     } catch {
       case e:Exception => {
-        logger.error("Error in: {}", stmt)
-        writeRequestData(session, attr.tag, start, nowMillis, start, nowMillis, KO, Some(e.getMessage()), List(stmt.toString()))
+        logger.error(s"Error in: $stmt")
+        writeRequestData(session, attr.tag, start, nowMillis, session.startDate, nowMillis, KO, Some(e.getMessage()), List(stmt.toString()))
         next ! session.markAsFailed
-        Failure(e.getMessage())
+        s"Error in: $stmt".failure
       }
     }
   }
