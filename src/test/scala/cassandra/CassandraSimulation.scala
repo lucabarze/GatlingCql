c8c3e1ab25685afab61914038eaecfd3a8072204
Mikhail Stepura <mikhail.stepura@outlook.com>
2015-11-03 16:32:50 -0800

    Updated to Gatling 2.1.7 and Scala 2.11.7

--- src/test/scala/cassandra/CassandraSimulation.scala
+++ src/test/scala/cassandra/CassandraSimulation.scala
@@ -1,27 +1,24 @@
-/*
- * #%L
- * GatlingCql
- * %%
- * Copyright (C) 2014 Mikhail Stepura
- * %%
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- * 
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- * 
+/**
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2015 Mikhail Stepura
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- * #L%
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 package cassandra
 
@@ -39,13 +36,15 @@ class CassandraSimulation extends Simulation {
   val keyspace = "test"
   val table_name = "test_table"
   val session = Cluster.builder().addContactPoint("127.0.0.1")
-                  .build().connect(s"$keyspace") //Your C* session
-  val cqlConfig = cql.session(session) //Initialize Gatling DSL with your session
-
-  //Setup
+                  .build().connect() //Your C* session
   session.execute(s"""CREATE KEYSPACE IF NOT EXISTS $keyspace 
                       WITH replication = { 'class' : 'SimpleStrategy', 
                                           'replication_factor': '1'}""")
+                                          
+  session.execute(s"USE $keyspace")
+  val cqlConfig = cql.session(session) //Initialize Gatling DSL with your session
+
+  //Setup
   session.execute(s"""CREATE TABLE IF NOT EXISTS $table_name (
           id timeuuid,
           num int,
